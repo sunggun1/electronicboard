7b06aeb2ac9b355dceb7b276efdfca1824b691c4
sunggun1 <sunggun1@onoffmix.com>
2020-01-01 22:45:05 +0900

    [mod] SK_20200101
    - STOP 기능 추가

--- app/src/main/java/com/example/SK_20191214/MainActivity.java
+++ app/src/main/java/com/example/SK_20191214/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.SK_20191214;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
@@ -15,29 +18,41 @@ import android.view.animation.AnimationSet;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 import static android.text.TextUtils.TruncateAt.MARQUEE;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
-    Button left,right,plus,minus;
+    Button left, right, plus, minus, textColor, backColor, blinkButton, stopButton;
     EditText TextEditView;
     int TextSize = 3;
-    Animation animationToLeft,animationToRight,animationToLeftFirst,animationToRightFirst;
-    AnimationSet animationSetLeft,animationSetRight;
+    int textColorValue, backColorValue;
+    LinearLayout textViewLayout;
+    Animation animationToLeft, animationToRight, animationToLeftFirst, animationToRightFirst;
+    AnimationSet animationSetLeft, animationSetRight;
+    Thread blinkThread;
+    boolean stopFlag = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView)findViewById(R.id.text);
-        plus = (Button)findViewById(R.id.plus);
-        minus = (Button)findViewById(R.id.minus);
+        textViewLayout = (LinearLayout) findViewById(R.id.textViewLinearLayout);
+        textView = (TextView) findViewById(R.id.text);
+        plus = (Button) findViewById(R.id.plus);
+        minus = (Button) findViewById(R.id.minus);
 
-        left = (Button)findViewById(R.id.left);
-        right = (Button)findViewById(R.id.right);
+        left = (Button) findViewById(R.id.left);
+        right = (Button) findViewById(R.id.right);
 
-        TextEditView = (EditText)findViewById(R.id.ChangeText);
+        textColor = (Button) findViewById(R.id.text_color);
+        backColor = (Button) findViewById(R.id.back_color);
+
+        TextEditView = (EditText) findViewById(R.id.ChangeText);
 
         ButtonSizeChange ButtonSizeChange = new ButtonSizeChange();
         ButtonMove ButtonMove = new ButtonMove();
@@ -54,18 +69,18 @@ public class MainActivity extends AppCompatActivity {
         int locationX = textView.getWidth();
         int locationY = textView.getHeight();
 
-        animationToLeftFirst = new TranslateAnimation(locationX/2,-800,0,0);
+        animationToLeftFirst = new TranslateAnimation(locationX / 2, -800, 0, 0);
         animationToLeftFirst.setDuration(6000);
 
-        animationToRightFirst = new TranslateAnimation(locationX/2,1200,0,0);
+        animationToRightFirst = new TranslateAnimation(locationX / 2, 1200, 0, 0);
         animationToRightFirst.setDuration(6000);
 
-        animationToLeft = new TranslateAnimation(1200,-800,0,0);
+        animationToLeft = new TranslateAnimation(1200, -800, 0, 0);
         animationToLeft.setDuration(12000);
         animationToLeft.setRepeatMode(Animation.RESTART);
         animationToLeft.setRepeatCount(Animation.INFINITE);
 
-        animationToRight = new TranslateAnimation(-800,1200, 0, 0);
+        animationToRight = new TranslateAnimation(-800, 1200, 0, 0);
         animationToRight.setDuration(12000);
         animationToRight.setRepeatMode(Animation.RESTART);
         animationToRight.setRepeatCount(Animation.INFINITE);
@@ -76,18 +91,61 @@ public class MainActivity extends AppCompatActivity {
         animationSetLeft.addAnimation(animationToLeft);
 
 
-
-
         animationSetRight.addAnimation(animationToRightFirst);
         animationSetRight.addAnimation(animationToRight);
 
+        ChangeColor ChangeColor = new ChangeColor();
+        textColor.setOnClickListener(ChangeColor);
+        backColor.setOnClickListener(ChangeColor);
+
+
+        textColorValue = ContextCompat.getColor(MainActivity.this, R.color.colorPrimary);
+        backColorValue = ContextCompat.getColor(MainActivity.this, R.color.colorPrimary);
+
+        blinkButton = (Button) findViewById(R.id.blink);
+        blinkButton.setOnClickListener(new blink());
+
+        stopButton = (Button) findViewById(R.id.stop);
+        stopButton.setOnClickListener(new BlinkStop());
+
     }
 
-    class ButtonMove implements View.OnClickListener{
+    class BlinkStop implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            stopFlag = true;
+            Log.d("",Boolean.toString(stopFlag));
+            textView.clearAnimation();
+            textView.setGravity(Gravity.CENTER);
+        }
+    }
+
+    public void openColorPicker(final int id) {
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, textColorValue, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                if (id == R.id.text_color) {
+                    textColorValue = color;
+                    textView.setTextColor(textColorValue);
+                } else if (id == R.id.back_color) {
+                    backColorValue = color;
+                    textViewLayout.setBackgroundColor(backColorValue);
+                }
+            }
+        });
+        colorPicker.show();
+    }
+
+    class ButtonMove implements View.OnClickListener {
         @Override
         public void onClick(View view) {
             int id = view.getId();
-            switch (id){
+            switch (id) {
                 case R.id.left:
                     textView.setGravity(Gravity.CENTER);
                     textView.startAnimation(animationSetLeft);
@@ -118,20 +176,82 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-    class ButtonSizeChange implements View.OnClickListener{
+    class ButtonSizeChange implements View.OnClickListener {
         @Override
         public void onClick(View view) {
             int id = view.getId();
-            switch (id){
+            switch (id) {
                 case R.id.plus:
-                    textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,textView.getTextSize()+TextSize);
+                    textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textView.getTextSize() + TextSize);
                     break;
                 case R.id.minus:
-                    textView.setTextSize(TypedValue.COMPLEX_UNIT_PX,textView.getTextSize()-TextSize);
+                    textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textView.getTextSize() - TextSize);
                     break;
             }
         }
     }
+
+    class ChangeColor implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            int id = view.getId();
+            switch (id) {
+                case R.id.text_color:
+                    openColorPicker(id);
+                    break;
+                case R.id.back_color:
+                    openColorPicker(id);
+                    break;
+            }
+        }
+    }
+
+    class blink implements View.OnClickListener {
+        private void blink() {
+            final Handler handler = new Handler();
+            Log.v("",Boolean.toString(stopFlag));
+            if (!stopFlag) {
+                blinkThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int timeToBlink = 500;
+                        try {
+                            blinkThread.sleep(timeToBlink);
+                        } catch (Exception e) {
+                        }
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (textView.getVisibility() == View.VISIBLE) {
+                                    textView.setVisibility(View.INVISIBLE);
+                                } else {
+                                    textView.setVisibility(View.VISIBLE);
+                                }
+                                blink();
+                            }
+                        });
+                    }
+                });
+                blinkThread.start();
+            } else {
+                Log.v("",Boolean.toString(stopFlag));
+                blinkThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setVisibility(View.VISIBLE);
+                    }
+                });
+                blinkThread.interrupt();
+                textView.setVisibility(View.VISIBLE);
+                stopFlag = false;
+            }
+        }
+
+        @Override
+        public void onClick(View view) {
+            blink();
+        }
+    }
 }
+
 
